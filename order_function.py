d726f07d355160caaa7a2ae817d072c3e814a3c2
vitalii.loboda <vitalii.loboda@gmail.com>
2021-01-08 14:42:03 +0200

    add changes

--- order_function.py
+++ order_function.py
@@ -3,7 +3,7 @@ import time
 
 DESKTOP_PATH = Path.home() / 'Desktop'
 IGNORE_LIST = ['Клиенты', 'Адреса клиентов', 'Клиенты_компании', 'Инструкции', 'Договора', 'Таблицы']
-DAYS_SECONDS = 30 * 24 * 60 * 60
+DAYS = 30
 
 
 def days_to_seconds(days: int) -> int:
@@ -13,7 +13,7 @@ def days_to_seconds(days: int) -> int:
 def process_file(file: Path, folder: str):
     if file.stem in IGNORE_LIST:
         print(f'*ignored {file.name}')
-    elif file.stat().st_mtime > (time.time() - DAYS_SECONDS):
+    elif file.stat().st_mtime > (time.time() - days_to_seconds(DAYS)):
         print(f'*ignored {file.name} with date: {time.ctime(file.stat().st_mtime)}')
     else:
         print(file.name)
@@ -27,73 +27,83 @@ def process_file(file: Path, folder: str):
             print(f'*folder {folder} created')
 
 
-def order_suffix(suffix, folder):
+def order_suffix(suffix: str, folder: str):
     file_list = DESKTOP_PATH.glob(f'*.{suffix}')
     for file in file_list:
         process_file(file, folder)
 
 
-def order_any(search_query, folder):
+def order_any(search_query: str, folder: str):
     file_list = [f for f in DESKTOP_PATH.iterdir() if f.is_file()]
-
     for file in file_list:
         file_l = file.name.lower()
         if file_l.find(search_query) >= 0:
             process_file(file, folder)
 
 
-def order(file_list, folder):
-    for file in file_list:
-        process_file(file, folder)
+# def order(file_list, folder):
+#     for file in file_list:
+#         process_file(file, folder)
 
 
-def remove_file(rule):
+def remove_file(search_query: str):
     file_list: list[Path] = [f for f in DESKTOP_PATH.iterdir() if f.is_file()]
 
     for file in file_list:
         file_l = file.name.lower()
-        if file_l.find(rule) >= 0:
+        if file_l.find(search_query) >= 0:
             if file.stem in IGNORE_LIST:
                 print(f'*ignored {file.name}')
-            elif file.stat().st_mtime > (time.time() - DAYS_SECONDS):
+            elif file.stat().st_mtime > (time.time() - days_to_seconds(DAYS)):
                 print(f'*ignored {file.name} with date: {time.ctime(file.stat().st_mtime)}')
             else:
                 print(f'*removed {file.name}')
                 file.unlink()
 
 
+def process_file2(file: Path, folder: str, function):
+    if file.stem in IGNORE_LIST:
+        print(f'*ignored {file.name}')
+    elif file.stat().st_mtime > (time.time() - days_to_seconds(DAYS)):
+        print(f'*ignored {file.name} with date: {time.ctime(file.stat().st_mtime)}')
+    else:
+        print(file.name)
+        function(file, folder)
+
+
+def rename_file(file, folder):
+    try:
+        Path(file).rename(Path(DESKTOP_PATH, folder, file.name))
+    except FileExistsError:
+        Path(file).rename(Path(DESKTOP_PATH, folder, f'new_{file.name}'))
+    except FileNotFoundError:
+        Path(DESKTOP_PATH, folder).mkdir()
+        Path(file).rename(Path(DESKTOP_PATH, folder, f'new_{file.name}'))
+        print(f'*folder {folder} created')
+
+
 def main():
-    rules_dict = {
+    suffixes_dict = {
         "Archive": 'zip rar 7zip arj'.split(),
-        'docx_files': 'docx doc docm'.split()
+        'docx_files': 'docx doc rtf'.split(),
+        'pdf_files': 'pdf'.split(),
+        'txt_files': 'txt xml'.split(),
+        'xlsx_files': 'xlsx xls'.split(),
+        'pictures': 'png jpeg gif jpg tiff'.split()
     }
 
-    superjob_folder = 'S'
-    rabota_ru_folder = 'R'
-    zarplata_ru_folder = 'Z'
-    archive_folder = 'Archive'
-    pdf_folder = 'pdf_files'
-    txt_folder = 'txt_files'
-    docx_folder = 'docx_files'
-    xlsx_folder = 'xlsx_files'
-    pictures_folder = 'pictures'
+    search_queries_dict = {
+        'S': 'superjob.ru',
+        'Z': 'zarplata.ru',
+        'R': 'rabota.ru'
+    }
 
-    order_any('superjob.ru', superjob_folder, IGNORE_LIST, days)
-    order_any('rabota.ru', rabota_ru_folder, IGNORE_LIST, days)
-    order_any('zarplata.ru', zarplata_ru_folder, IGNORE_LIST, days)
+    for folder, query in search_queries_dict.items():
+        order_any(query, folder)
 
-    for folder, suffixes in rules_dict.items():
+    for folder, suffixes in suffixes_dict.items():
         for suffix in suffixes:
             order_suffix(suffix, folder)
-
-    # order_suffix('zip', archive_folder, IGNORE_LIST, days)
-    # order_suffix('pdf', pdf_folder, IGNORE_LIST, days)
-    # order_suffix('txt', txt_folder, IGNORE_LIST, days)
-    # order_suffix('docx', docx_folder, IGNORE_LIST, days)
-    # order_suffix('xlsx', xlsx_folder, IGNORE_LIST, days)
-    # order_suffix('doc', docx_folder, IGNORE_LIST, days)
-    # order_suffix('rtf', docx_folder, IGNORE_LIST, days)
-    # order_suffix('png', pictures_folder, IGNORE_LIST, days)
 
 
 if __name__ == '__main__':
